d9a5c590551bb7f0265b50f0c9a1d0b9f14b3213
realstevelee <realstevelee@gmail.com>
2020-12-17 00:44:01 -0500

    configure database

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrpyt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcript.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcript.hashSync('123456', 10)
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password: bcript.hashSync('123456', 10)
+    }
+]
+
+export default users
