23fdd0ce6fcad4983905576922dd85d80d109665
realstevelee <realstevelee@gmail.com>
2020-12-21 02:05:18 -0500

    modified backend server config

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
 
